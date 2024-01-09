a7aa6a0bf2c4c50d18c32b44c39532ca75e411aa
Shanki200801 <shashank200801@gmail.com>
2024-01-09 14:19:34 +0530

    modal change and no cache

--- next.config.js
+++ next.config.js
@@ -8,6 +8,28 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "vercel.com"],
   },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-cache, no-store, must-revalidate",
+          },
+          {
+            key: "Pragma",
+            value: "no-cache",
+          },
+          {
+            key: "Expires",
+            value: "0",
+          },
+        ],
+      },
+    ];
+  },
   async redirects() {
     return [
       {
